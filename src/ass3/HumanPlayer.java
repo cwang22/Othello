92df50c1d306b7e30f84cb46b6d8909c93b1e0a6
C.Wang <wcg0304@sina.com>
2014-05-16 10:48:29 +1000

    add AIPlayer.java add HumanPlayer.java

---
+++ src/ass3/HumanPlayer.java
@@ -0,0 +1,13 @@
+package ass3;
+
+public class HumanPlayer extends Player {
+
+  public HumanPlayer(Cell c, String name) {
+    super(c, name);
+  }
+
+  public String input() {
+    return super.input();
+  }
+
+}
